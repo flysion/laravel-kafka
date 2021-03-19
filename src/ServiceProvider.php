e85b5ab4d88b8740bd445b3b091fe0ee8ad4d65d
李星星 <sss60@qq.com>
2021-03-19 15:43:20 +0800

    update

---
+++ src/ServiceProvider.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace Flysion\Kafka;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider implements DeferrableProvider
+{
+    protected $commands = [
+        Commands\HighConsumer::class
+    ];
+
+    /**
+     * Bootstrap any application services.
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+    /**
+     * Register any application services.
+     * @return void
+     */
+    public function register()
+    {
+        $this->commands($this->commands);
+
+        $this->app->singleton('kafka.consumer', ConsumerManager::class);
+        $this->app->singleton('kafka.highconsumer', HighConsumerManager::class);
+        $this->app->singleton('kafka.producer', ProducerManager::class);
+
+        $this->publishes([
+            __DIR__ . '/../config/kafka.php' => $this->app->configPath('kafka.php')
+        ], 'kafka');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'kafka.consumer',
+            'kafka.highconsumer',
+            'kafka.producer'
+        ];
+    }
+}
